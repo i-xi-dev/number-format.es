12b1bb63a9b62218253078cc822865ebbb012e25
i-xi-dev <naoyuki.isono@gmail.com>
2024-06-12 21:07:30 +0900

    1.0.3

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@ export {
   NumberEx,
   Radix,
   SafeInteger,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.0/mod.ts";
-export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.5/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.1/mod.ts";
+export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/7.0.0/mod.ts";
